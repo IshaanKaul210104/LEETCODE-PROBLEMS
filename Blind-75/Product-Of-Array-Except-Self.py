fb79222daf613d36a24dc3bc5f8eea161c846b33
Ishaan Kaul <ishaank402@gmail.com>
2026-03-17 20:15:18 +0530

    .

---
+++ Blind-75/Product-Of-Array-Except-Self.py
@@ -0,0 +1,14 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        left = [1]*(len(nums) + 1)
+        right = [1]*(len(nums) + 1)
+
+        for i in range(1, len(nums) + 1):
+            left[i] = left[i - 1]*nums[i]
+        for i in range(len(nums), -1, -1):
+            right[i] = right[i + 1]*nums[i]
+
+        for i in range(len(nums)):
+            nums[i] = left[i]*right[i]
+
+        return nums
--- Blind-75/Top-K-Frequent-Elements.py
+++ Blind-75/Top-K-Frequent-Elements.py
@@ -1,18 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        heap = []
-        m = Counter(nums)
+        count = Counter(nums)
+        freq = [[] for i in range(len(nums) + 1)]
 
-        for val, freq in m.items():
-            if len(heap) < k:
-                heapq.heappush(heap, (freq, val))
-            else:
-                if freq > heap[0][0]:
-                    heapq.heappop(heap)
-                    heapq.heappush(heap, (freq, val))
+        for n, c in count.items():
+            freq[c].append(n)
 
-        ans = []
-        while heap:
-            ans.append(heapq.heappop(heap)[1])
-
-        return ans
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
