d13eebb0e2ab7ee5d8e5b5c0f3714cac683b93c2
Ishaan Kaul <ishaank402@gmail.com>
2026-03-28 11:19:46 +0530

    .

--- Blind-75/Reorder-Linked-List.py
+++ Blind-75/Reorder-Linked-List.py
@@ -6,35 +6,22 @@
 
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        def rev(head) -> Optional[ListNode]:
-            prev = nxt = None
-            temp = head
-            while temp:
-                nxt = temp.next
-                temp.next = prev
-                prev = temp
-                temp = nxt
-            return prev
-
-        slow = fast = head
-        while fast.next and fast.next.next:
+        slow, fast = head, head.next
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
 
-        firhead = head
-        sechead = rev(slow.next)
-        dummy = ListNode()
-        curr = dummy
-        t1, t2 = firhead, sechead
-        while t1 and t2:
-            curr.next = t1
-            curr = curr.next
-            t1 = t1.next
-            curr.next = t2
-            curr = curr.next
-            t2 = t2.next
-        if t1:
-            curr.next = t1
-        elif t2:
-            curr.next = t2
-        return dummy.next
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
