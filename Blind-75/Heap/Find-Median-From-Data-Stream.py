f2bd5e92de5accd295ea665553f4b8fa5256bc74
Ishaan Kaul <ishaank402@gmail.com>
2026-03-30 14:56:41 +0530

    .

---
+++ Blind-75/Heap/Find-Median-From-Data-Stream.py
@@ -0,0 +1,16 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.l = []
+
+    def addNum(self, num: int) -> None:
+        self.l.append(num)
+        self.l.sort()  # keep sorted
+
+    def findMedian(self) -> float:
+        n = len(self.l)
+
+        if n % 2 == 0:
+            return (self.l[(n - 1)//2] + self.l[n//2]) / 2
+        else:
+            return self.l[n//2]
